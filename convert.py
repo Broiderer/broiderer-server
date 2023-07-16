3c38225ca9c1db2955bcf665d05bf870cccc1b0b
Guillaume Meigniez <guillaume.meigniez@gmail.com>
2023-07-16 13:47:32 +0200

    Update convert subprocess syntax

--- convert.py
+++ convert.py
@@ -2,14 +2,14 @@ import subprocess
 
 def pes_to_svg(input_path, output_path):
     subprocess.run([
-        'vpype', 'eread', input_path, 
+        'vpype', 'eread', input_path,
         'linemerge', 'linesort', 'reloop', 'linesimplify',
         'write', output_path
     ])
 
 def svg_to_pes(input_path, output_path):
     subprocess.run([
-        'vpype', 'read', input_path, 
-        'efill', '-d 5px', '-t 1px' 
+        'vpype', 'read', input_path,
+        'efill', '-d', '5px', '-t', '3px',
         'ewrite', output_path
     ])
